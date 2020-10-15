92288a96901c464bbe7821163442c471f8fe5638
Ilia Yusov <ouster2k@gmail.com>
2020-10-15 18:29:59 +0300

    module design second iteration

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
 import requests
 from datetime import date
-import json
 import os
-import tqdm
+from tqdm import tqdm
 
 
 class VkAPI:
@@ -10,28 +9,33 @@ class VkAPI:
     TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
     VER = '5.124'
 
-    def __init__(self, screen_name: str):
+    def __init__(self, name):
         self.token = VkAPI.TOKEN
-        self.name = screen_name
         self.ver = VkAPI.VER
+        self.id = self.get_id(name)
 
-    def get_id(self, screen_name):  # проверить screen_name или id
-        endpoint = '/utils.resolveScreenName'
+    def get_id(self, name):
+        """получаем id если пользователь ввел screen_name"""
+        endpoint = '/users.get'
         kwargs = {
-            'screen_name': screen_name,
+            'user_ids': name,
             'access_token': self.token,
             'v': self.ver
         }
         response = requests.get(VkAPI.API_URL + endpoint, params=kwargs)
         response.raise_for_status()
-        if response.json()['response']:
-            return str(response.json()['response']['object_id'])
-        return ''  # хрень
+        if 'error' in response.json():
+            raise requests.RequestException(response.json()['error']['error_msg'])
+        return response.json()['response'][0]['id']
 
     def get_photo_list(self):
+        """
+        получаем список фотографий профиля,
+        выбираем максимальные размеры по типу,
+        """
         endpoint = '/photos.get'
         kwargs = {
-            'owner_id': self.get_id(self.name),
+            'owner_id': self.id,
             'album_id': 'profile',
             'extended': '1',
             'photo_sizes': '1',
@@ -40,29 +44,58 @@ class VkAPI:
         }
         response = requests.get(VkAPI.API_URL + endpoint, params=kwargs)
         response.raise_for_status()
+        if 'error' in response.json():
+            raise requests.RequestException(response.json()['error']['error_msg'])
         photo_list = []
-        like_list = []
-        for item in response.json()['response']['items']:
+        for item in tqdm(response.json()['response']['items'], desc='Getting photos URLs'):
             max_not_found = True
             for type_ in 'wzyxms':
                 for size in item['sizes']:
                     if max_not_found and type_ == size['type']:
                         max_not_found = False
                         photo = {'likes': item['likes']['count'], 'date': item['date'], 'type': size['type'],
-                                 'url': size['url']}
+                                 'url': size['url'], 'id': item['id']}
                         photo_list.append(photo)
-                        like_list.append(item['likes']['count'])
                         break
+        return VkAPI._add_names(photo_list)
+
+    @staticmethod
+    def _add_names(photo_list):
+        """создаем имена файлов из количества лайков, даты создания и, в крайнем случае, id"""
+        names = []
         for photo in photo_list:
-            if like_list.count(photo['likes']) == 1:
-                photo['file_name'] = f'{photo["likes"]}.{photo["url"].split("?")[0].rsplit(".")[-1]}'
-            else:
-                photo['file_name'] = f'{photo["likes"]}_{date.fromtimestamp(photo["date"])}.{photo["url"].split("?")[0].rsplit(".")[-1]}'
+            file_name = f'{photo["likes"]}.' \
+                        f'{photo["url"].split("?")[0].rsplit(".")[-1]}'
+            if file_name in names:
+                file_name = f'{photo["likes"]}_' \
+                            f'{date.fromtimestamp(photo["date"])}.' \
+                            f'{photo["url"].split("?")[0].rsplit(".")[-1]}'
+            if file_name in names:
+                file_name = f'{photo["likes"]}_' \
+                            f'{date.fromtimestamp(photo["date"])}_' \
+                            f'{photo["id"]}.' \
+                            f'{photo["url"].split("?")[0].rsplit(".")[-1]}'
+            photo['file_name'] = file_name
+            names.append(file_name)
         return photo_list
 
 
 class YandexAPI:
     API_URL = 'https://cloud-api.yandex.net/v1/disk/resources'
+    API_ERRORS = {
+        200: "OK",
+        201: "OK",
+        400: "Некорректные данные",
+        401: "Не авторизован",
+        403: "API недоступно",
+        404: "Не удалось найти запрошенный ресурс",
+        406: "Ресурс не может быть представлен в запрошенном формате",
+        409: "Ресурс уже существует",
+        423: "Ресурс заблокирован",
+        429: "Слишком много запросов",
+        503: "Сервис временно недоступен",
+        507: "Недостаточно свободного места"
+    }
 
     def __init__(self, token: str):
         self.token = token
@@ -73,12 +106,13 @@ class YandexAPI:
         auth_header = {'Authorization': self.token}
         kwargs = {'path': '/' + file_path.rsplit(os.path.sep)[-1]}
         upload_response = requests.get(YandexAPI.API_URL + endpoint, headers=auth_header, params=kwargs)
-        upload_response.raise_for_status()
+        if upload_response.status_code != 200:
+            return upload_response.status_code
         response = requests.put(upload_response.json()['href'], data)
         response.raise_for_status()
         return response.status_code
 
-    def create_folder(self, folder=f'VK_photos_{date.today()}'):
+    def create_folder(self, folder):
         """Метод создает папку на яндекс диск. По умолчанию VK_photos+<сегодняшняя дата>"""
         auth_header = {'Authorization': self.token}
         kwargs = {'path': folder}
@@ -86,27 +120,9 @@ class YandexAPI:
         response.raise_for_status()
         return f'Folder {kwargs["path"]} created'
 
-
-def write_list_to_file(photo_list: list, yandex_handler: YandexAPI, folder=f'VK_photos_{date.today()}'):
-    photo_files = []
-    for photo in photo_list:
-        response = requests.get(photo['url'])
-        img = response.content
-        if response.status_code == 200:
-            yandex_handler.upload(folder + '/' + photo['file_name'], img)
-            print(f'{photo["file_name"]:_<20}Ok')
-            photo_files.append({'file_name': photo['file_name'], 'size': photo['type']})
-        else:
-            print(f'{photo["file_name"]:_<20}{response.text.split("<title>")[1].split("</title>")[0]}')
-    with open('photos.json', 'w', encoding='utf-8') as f:
-        json.dump(photo_files, f, indent=2)
-
-
-if __name__ == '__main__':
-    yandex = YandexAPI('<TOKEN>')
-    vk = VkAPI('shadow_tm')
-    try:
-        yandex.create_folder()
-        write_list_to_file(vk.get_photo_list(), yandex)
-    except requests.HTTPError as http_error:
-        print(http_error)
+    def object_exist(self, path):
+        """Метод возвращает True, если объект уже существует"""
+        auth_header = {'Authorization': self.token}
+        kwargs = {'path': path}
+        response = requests.get(YandexAPI.API_URL, headers=auth_header, params=kwargs)
+        return True if response.status_code == 200 else False
